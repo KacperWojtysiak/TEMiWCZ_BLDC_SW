f09e5deb79dc4bee9a9bbda3d38102c12fcc3bb0
MicBog <80053410+Bognacy@users.noreply.github.com>
2025-05-20 21:24:41 +0200

    usb recive callback echo

--- Core/Src/USB.c
+++ Core/Src/USB.c
@@ -20,3 +20,19 @@ void USB_Send(){
   CDC_Transmit_FS((uint8_t*)"Hello", 5);
   printf("siema\n");
 }
+
+uint8_t rx_buf[64];
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+uint8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
+{
+    memcpy(rx_buf, Buf, *Len);
+    rx_buf[*Len] = '\0';  // jeśli dane są tekstowe
+
+    // Prosty debug – echo
+    CDC_Transmit_FS(rx_buf, *Len);
+
+    USBD_CDC_SetRxBuffer(&hUsbDeviceFS, Buf);
+    USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+    return (USBD_OK);
+}
